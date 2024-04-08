e66065f119808dc6da1644ba793eb8985451103e
Julia Chebotarova <chebotarova.pn@ucu.edu.ua>
2024-04-08 20:14:25 +0300

    Done "Linked Lists - Sorted Insert", the sixth task

---
+++ Sorted Insert/sorted.py
@@ -0,0 +1,26 @@
+"""
+Linked Lists - Sorted Insert.
+"""
+
+class Node(object):
+    """
+    Node.
+    """
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def sorted_insert(head, data):
+    """
+    Sorted
+    """
+    new_node = Node(data)
+    if not head or data < head.data:
+        new_node.next = head
+        return new_node
+    current = head
+    while current.next and current.next.data < data:
+        current = current.next
+    new_node.next = current.next
+    current.next = new_node
+    return head
